1a04bc348462d27d0c7987fe706bfb131bc487ed
Ted CAI <ted.xmdgj@gmail.com>
2015-03-01 21:35:53 +0000

    Bug Fixed
    Fixed bug: cannot trigger the damage function when projectile hits the
    enemy.

--- Assets/Strike.cs
+++ Assets/Strike.cs
@@ -3,21 +3,22 @@ using System.Collections;
 
 public class Strike : MonoBehaviour {
 
-	public float speed;
+	//public float speed;
 	public int damage;
-	public GameObject opponent;
+	//public GameObject opponent;
 	// Use this for initialization
 	void Start () {
+		Debug.Log ("s");
 		//transform.eulerAngles = opponent.GetComponent<Fighter> ().transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (Vector3.forward*speed*Time.deltaTime);
+		//transform.Translate (Vector3.forward*speed*Time.deltaTime);
 	}
 
 	void OnTriggerEnter(Collider other){
-		//Debug.Log ("test");
+		Debug.Log ("hit s");
 		if (other.tag == "Enemy") {
 			other.GetComponent<Mob>().getHit (damage);
 		}
--- Assets/StrikeFireBall.cs
+++ Assets/StrikeFireBall.cs
@@ -5,19 +5,21 @@ public class StrikeFireBall : MonoBehaviour {
 
 	public float speed;
 	public int damage;
-
+	private float timeKill;
 	// Use this for initialization
 	void Start () {
+		timeKill = Time.time;
 		transform.Rotate(new Vector3(0f,270f,0f));
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Translate (Vector3.right*speed*Time.deltaTime);
+		if ( Time.time > timeKill + 3 ) Destroy (gameObject);
 	}
 
 	void OnTriggerEnter(Collider other){
-		//Debug.Log ("test");
+		Debug.Log ("test");
 		if (other.tag == "Enemy") {
 			other.GetComponent<Mob>().getHit (damage);
 		}
