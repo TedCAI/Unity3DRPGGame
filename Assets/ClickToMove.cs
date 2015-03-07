8bbc7084745e5ff7275c9bdd0c15147704022ce2
Ted CAI <ted.xmdgj@gmail.com>
2015-03-07 00:05:50 +0000

    Fix Bug
    - Rewrite functions for control character with keyboard
    - Enable vectors combination, for example, when press W and D at the
    same time, character will move along top-right direction

--- Assets/ClickToMove.cs
+++ Assets/ClickToMove.cs
@@ -14,6 +14,13 @@ public class ClickToMove : MonoBehaviour {
 	public static bool isRunning =false;
 
 	public static Vector3 cursorPosition;
+
+	public static bool wDown = false;
+	public static bool sDown = false;
+	public static bool aDown = false;
+	public static bool dDown = false;
+
+	public static int numberOfKeys = 0;
 	// Use this for initialization
 	void Start () {
 		position = transform.position;
@@ -24,43 +31,84 @@ public class ClickToMove : MonoBehaviour {
 		//Debug.Log (die);
 		locateCursor ();
 		if (!attack && !die) {
-						if (Input.GetMouseButton (0)) {
-								locatePosition ();
+			if (Input.GetMouseButton (0)) {
+				locatePosition ();
 								//con = 1;
-						}
+			}
 						//if (con == 1) {
-						moveToPosition ();
+			moveToPosition ();
 
-					if(Input.GetKey(KeyCode.W)){
-						//Debug.Log("triggered");
-						if(isRunning){
-							position = transform.position;
-							isRunning = false;
-						}
-						moveForward(KeyCode.W);
-					}else if(Input.GetKey(KeyCode.S)){
-							//Debug.Log("triggered");
-							if(isRunning){
-								position = transform.position;
-								isRunning = false;
-							}
-							moveForward(KeyCode.S);
-						}else if(Input.GetKey(KeyCode.A)){
-							//Debug.Log("triggered");
-							if(isRunning){
-								position = transform.position;
-								isRunning = false;
-							}
-							moveForward(KeyCode.A);
-						}else if(Input.GetKey(KeyCode.D)){
-							//Debug.Log("triggered");
-							if(isRunning){
-								position = transform.position;
-								isRunning = false;
-							}
-							moveForward(KeyCode.D);
-						}
+			if(Input.GetKeyDown(KeyCode.W) && !wDown && numberOfKeys <= 2 && !sDown){
+				wDown = true;
+				numberOfKeys+=1;
+				if(isRunning){
+					position = transform.position;
+					isRunning = false;
 				}
+						//moveForward(KeyCode.W);
+			}if(Input.GetKeyDown(KeyCode.S) && !sDown && numberOfKeys <= 2 && !wDown){
+				sDown = true;
+				numberOfKeys += 1;
+				if(isRunning){
+					position = transform.position;
+					isRunning = false;
+				}
+				//moveForward(KeyCode.S);
+			}if(Input.GetKeyDown(KeyCode.A) && !aDown && numberOfKeys <= 2 && !dDown){
+				aDown = true;
+				numberOfKeys+=1;
+				if(isRunning){
+					position = transform.position;
+					isRunning = false;
+				}
+							//moveForward(KeyCode.A);
+			}if(Input.GetKeyDown(KeyCode.D) && !dDown && numberOfKeys <= 2 && !aDown){
+				dDown = true;
+				numberOfKeys+=1;
+				if(isRunning){
+					position = transform.position;
+					isRunning = false;
+				}
+							//moveForward(KeyCode.D);
+			}
+
+			if(!Input.GetKey(KeyCode.W) && wDown && numberOfKeys <= 2 && numberOfKeys > 0){
+				wDown = false;
+				numberOfKeys -= 1;
+				if(isRunning){
+					position = transform.position;
+					isRunning = false;
+				}
+				//moveForward(KeyCode.W);
+			}if(!Input.GetKey(KeyCode.S) && sDown && numberOfKeys <= 2 && numberOfKeys > 0){
+				sDown = false;
+				numberOfKeys -= 1;
+				if(isRunning){
+					position = transform.position;
+					isRunning = false;
+				}
+				//moveForward(KeyCode.S);
+			}if(!Input.GetKey(KeyCode.A) && aDown && numberOfKeys <= 2 && numberOfKeys > 0){
+				aDown = false;
+				numberOfKeys -= 1;
+				if(isRunning){
+					position = transform.position;
+					isRunning = false;
+				}
+				//moveForward(KeyCode.A);
+			}if(!Input.GetKey(KeyCode.D) && dDown && numberOfKeys <= 2 && numberOfKeys > 0){
+				dDown = false;
+				numberOfKeys -= 1;
+				if(isRunning){
+					position = transform.position;
+					isRunning = false;
+				}
+				//moveForward(KeyCode.D);
+			}
+
+			moveForward();
+
+		}
 
 	}
 
@@ -110,8 +158,9 @@ public class ClickToMove : MonoBehaviour {
 
 	}
 
-	void moveForward(KeyCode key){
+	void moveForward(){
 				//Quaternion newRotation = Quaternion.Euler (new Vector3 (0f, 0f, 0f));
+		/*
 		if (key == KeyCode.W) {
 			transform.rotation = Quaternion.Euler (new Vector3 (0f, 0f, 0f));
 			controller.SimpleMove (transform.forward * 10f);
@@ -125,8 +174,29 @@ public class ClickToMove : MonoBehaviour {
 			transform.rotation = Quaternion.Euler (new Vector3 (0f, 90f, 0f));
 			controller.SimpleMove (transform.forward * 10f);
 		}
-		//Debug.Log (transform.position);
-		GetComponent<Animation>().CrossFade(run.name);
+		*/
+		if (numberOfKeys > 0) {
+						//Debug.Log (transform.position);
+			Vector3 newRotation = new Vector3(0f,0f,0f);
+			if(sDown)
+				newRotation.y += 180f;
+			if(dDown)
+				newRotation.y += 90f;
+			if(aDown){
+				newRotation.y = -90f;
+				if(wDown)
+					newRotation.y = -90f;
+				if(sDown)
+					newRotation.y = -270f;
+			}
+
+			Debug.Log(newRotation);
+			Debug.Log(numberOfKeys);
+			newRotation.y = newRotation.y / (float)numberOfKeys;
+			transform.rotation = Quaternion.Euler (newRotation);
+			controller.SimpleMove (transform.forward * 10f);
+			GetComponent<Animation> ().CrossFade (run.name);
+		}
 	}
 
 }
