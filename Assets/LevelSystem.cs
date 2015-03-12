d075513fcd98115698b1a9726cb5dc7005cade5c
Ted CAI <ted.xmdgj@gmail.com>
2015-03-12 22:55:51 +0000

    Add New Features
    - Add text for level system
    - Display "Level up!" when level up

--- Assets/LevelSystem.cs
+++ Assets/LevelSystem.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class LevelSystem : MonoBehaviour {
 
@@ -7,6 +8,8 @@ public class LevelSystem : MonoBehaviour {
 	public int exp;
 
 	public Fighter player;
+
+	public string records;
 
 	// Use this for initialization
 	void Start () {
@@ -22,14 +25,33 @@ public class LevelSystem : MonoBehaviour {
 		if (exp >= (100 * level)) {
 			exp -= (100 * level);
 			level += 1;
+			//GameObject.Find("Records").GetComponent<Text>().text += "\nLevel up!";
 			//exp = 0;
 			levelEffect();
 		}
 	}
 
 	void levelEffect(){
+		if(level > 1)
+			GameObject.Find("Records").GetComponent<Text>().text += "\nLevel up!";
 		player.maxHealth += 100;
 		player.damage += 50;
 		player.health = player.maxHealth;
+	}
+
+	void OnGUI(){
+		Vector3 screenCoordinate = Camera.main.WorldToScreenPoint (player.GetComponent<Transform>().position);
+		GUIStyle myStyle = new GUIStyle ();
+		//Debug.Log (screenCoordinate);
+		myStyle.fontSize = 16;
+		myStyle.normal.textColor = Color.white;
+		myStyle.alignment = TextAnchor.MiddleLeft;
+		Rect hpLocation = GameObject.Find ("Player HP").GetComponent<PlayerHP> ().framePosition;
+
+		//float scale = GameObject.Find ("Text").GetComponent<PlayerText> ().scaleX;
+		//GUI.Label (new Rect (screenCoordinate.x - 13f, Screen.height - screenCoordinate.y, 26f, 25f), level.ToString(), myStyle);
+		//GUI.Label (new Rect (hpLocation.x - hpLocation.width/2, hpLocation.y - hpLocation.height/2, 26f, 25f), level.ToString(), myStyle);
+		GUI.Label (new Rect (hpLocation.x - 20f, hpLocation.y - 10f, 26f, 25f), level.ToString(), myStyle);
+		//GUI.Box (new Rect (Screen.width - 350f, Screen.height - 300f, 350f, 300f), records);
 	}
 }
