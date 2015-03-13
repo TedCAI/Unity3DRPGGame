74287aff89f2db8bce6d459e111490e13ad1171f
Ted CAI <ted.xmdgj@gmail.com>
2015-03-13 22:57:40 +0000

    Add Cold-Down for Skills

--- Assets/SpecialAttack.cs
+++ Assets/SpecialAttack.cs
@@ -10,6 +10,8 @@ public class SpecialAttack : MonoBehaviour {
 	public bool inAction;
 	public GameObject particleEffect;
 	public int projectile;
+	public float coldDownTime;
+	private bool inCD;
 
 	public bool opponentBased;
 
@@ -23,45 +25,37 @@ public class SpecialAttack : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		/*
-		if (Input.GetKeyDown (key) && !player.specialAttack) {
-			player.resetAttackFunction();
-			player.specialAttack=true;
-			inAction=true;
-		}
-
-		//if(dotSkill != null){
-		//	dotSkill.GetComponent<DOTSkill>().damage = (int)(damagePercentage*100);
-		//}
-
-		if (inAction) {
-			if(player.attackFunction (stunTime, damagePercentage, key, particleEffect, projectile, opponentBased)){
-
-			}else{
-				inAction = false;
-			}
-		}*/
-
 		attack ();
 	}
 
 	public void attack(){
-		if (Input.GetKeyDown (key) && !player.specialAttack) {
+		if (Input.GetKeyDown (key) && !player.specialAttack && !inCD) {
 			player.resetAttackFunction ();
 			player.specialAttack = true;
 			inAction = true;
+			inCD = true;
+			IEnumerator cdTimer = SkillInCD(coldDownTime);
+			StartCoroutine (cdTimer);
 		}
 		
 				//if(dotSkill != null){
 				//	dotSkill.GetComponent<DOTSkill>().damage = (int)(damagePercentage*100);
 				//}
 		
-		if (inAction) {
+		if (inAction ) {
 			if (player.attackFunction (stunTime, damagePercentage, key, particleEffect, projectile, opponentBased)) {
-				
+
+				//IEnumerator cdTimer = SkillInCD(coldDownTime);
+				//StartCoroutine (cdTimer);
 			} else {
 				inAction = false;
 			}
 		}
+	}
+
+	IEnumerator SkillInCD(float cd){
+		yield return new WaitForSeconds (cd);
+		inCD = false;
+
 	}
 }
