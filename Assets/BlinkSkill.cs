cbe178f113e683d814184aed6514885550e00b37
Ted CAI <ted.xmdgj@gmail.com>
2015-03-13 22:33:40 +0000

    Fix Blink Bug and Add Particle for Blink
    - Fixed bug: player may teleport to unreachable location
    - Add particle for blink skill
    - Enable to set casting time for blink skill

--- Assets/BlinkSkill.cs
+++ Assets/BlinkSkill.cs
@@ -5,6 +5,8 @@ public class BlinkSkill : MonoBehaviour {
 
 	public GameObject player;
 	public KeyCode key;
+
+	public float particalSize = 0.25f;
 	// Use this for initialization
 	void Start () {
 		//player = GameObject.Find ("Player");
@@ -18,6 +20,7 @@ public class BlinkSkill : MonoBehaviour {
 
 	void blink(){
 		//player = GameObject.Find ("Player");
+		//activateEffect ();
 		Vector3 playerLocation = GetComponent<Transform> ().position;
 		Vector3 newLocation = playerLocation;
 		Vector3 mouseLocation = ClickToMove.cursorPosition;
@@ -30,7 +33,34 @@ public class BlinkSkill : MonoBehaviour {
 			newLocation = mouseLocation;
 		}
 
+		newLocation.y = Terrain.activeTerrain.SampleHeight (newLocation) + 0.1f;
+		//activateEffect ();
+		IEnumerator coroutine = Activation (1.5f, newLocation);
+		InvokeRepeating ("scaleSize", 0f, 0.1f);
+		GameObject.Find ("BlinkEffect2").GetComponent<ParticleRenderer> ().enabled = true;
+		StartCoroutine (coroutine);
+
+		//GetComponent<Transform> ().position = newLocation;
+		//GetComponent<CharacterController> ().SimpleMove (transform.up * 10f * Time.deltaTime);
+	}
+
+	IEnumerator Activation(float waitTime, Vector3 newLocation){
+		//yield return new WaitForSeconds (waitTime);
+		GameObject.Find ("BlinkEffect").GetComponent<MeshRenderer> ().enabled = true;
+		//InvokeRepeating ("scaleSize", 1f, 0.1f);
+		yield return new WaitForSeconds (waitTime);
 		GetComponent<Transform> ().position = newLocation;
 		GetComponent<CharacterController> ().SimpleMove (transform.up * 10f * Time.deltaTime);
+		CancelInvoke ("scaleSize");
+		particalSize = 0.25f;
+		GameObject.Find ("BlinkEffect2").GetComponent<ParticleRenderer> ().maxParticleSize = particalSize;
+		GameObject.Find ("BlinkEffect").GetComponent<MeshRenderer> ().enabled = false;
+		GameObject.Find ("BlinkEffect2").GetComponent<ParticleRenderer> ().enabled = false;
+	}
+
+	void scaleSize(){
+		particalSize += 0.1f;
+		Debug.Log (particalSize);
+		GameObject.Find ("BlinkEffect2").GetComponent<ParticleRenderer> ().maxParticleSize = particalSize;
 	}
 }
